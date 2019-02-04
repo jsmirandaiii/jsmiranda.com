137cfdeb23464faee540a682716ec955cd47c8b3
Jose Miranda III <jsmirandaiii@gmail.com>
2019-02-04 15:28:24 +0800

    Added node/express server

---
+++ index.js
@@ -0,0 +1,13 @@
+var express = require("express");
+var path = require("path");
+var app = express();
+var port = 3000 || process.env.PORT;
+
+app.use(express.static(path.resolve("./public/assets")));
+app.get("/", function(req, res) {
+  return res.sendFile(path.resolve("./public/index.html"));
+});
+app.listen(port, function(err) {
+  if (err) throw new Error(err);
+  console.log(`Application is running at port ${port}`);
+});
