ccdc21ed28370c33fd1252767eb8f646e7b4c351
Jose Miranda III <jsmirandaiii@gmail.com>
2019-05-30 06:28:43 +0800

    fixed security vulnerability in package-lock.json

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-require("dotenv").config();
-var express = require("express");
-var path = require("path");
-var helmet = require("helmet");
-var getENV = require("./helpers/getENV");
-var app = express();
+require('dotenv').config()
+var express = require('express')
+var path = require('path')
+var helmet = require('helmet')
+var getENV = require('./helpers/getENV')
+var app = express()
 
-var port = process.env.PORT || 3000;
-var mode = getENV();
+var port = process.env.PORT || 3000
+var mode = getENV()
 
-app.disable("x-powered-by");
-app.set("trust proxy", true);
+app.disable('x-powered-by')
+app.set('trust proxy', true)
 
 app.use(
   helmet.hsts({
@@ -17,29 +17,29 @@ app.use(
     includeSubDomains: true,
     preload: true,
     setIf: function(req, res) {
-      return req.secure;
+      return req.secure
     }
   }),
   function(req, res, next) {
-    if (!req.secure && (mode === "production" || mode === "maintenance")) {
-      return res.redirect(301, "https://" + req.headers.host + req.originalUrl);
+    if (!req.secure && (mode === 'production' || mode === 'maintenance')) {
+      return res.redirect(301, 'https://' + req.headers.host + req.originalUrl)
     }
-    next();
+    next()
   },
-  express.static(path.resolve("./public/assets"))
-);
+  express.static(path.resolve('./public/assets'))
+)
 
-if (mode === "maintenance") {
-  app.get("*", function(req, res) {
-    return res.sendFile(path.resolve("./public/maintenance.html"));
-  });
+if (mode === 'maintenance') {
+  app.get('*', function(req, res) {
+    return res.sendFile(path.resolve('./public/maintenance.html'))
+  })
 }
 
-app.get("/", function(req, res) {
-  return res.sendFile(path.resolve("./public/index.html"));
-});
+app.get('/', function(req, res) {
+  return res.sendFile(path.resolve('./public/index.html'))
+})
 
 app.listen(port, function(err) {
-  if (err) throw new Error(err);
-  console.log(`Application is running at port ${port}`);
-});
+  if (err) throw new Error(err)
+  console.log(`Application is running at port ${port}`)
+})
